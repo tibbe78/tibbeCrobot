60a3d6c06dc6d106e277f84c062b1261947b7106
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2017-05-05 07:31:55 +0200

    my obfuscate prog.
    a small bat to obfuscate my code.
    uses fnr
    https://findandreplace.codeplex.com/

---
+++ obfuscate.bat
@@ -0,0 +1,23 @@
+@Echo off
+cls
+
+for /f "tokens=1" %%I IN (c:\crobots\tibbe.txt) DO set tibbe=%%I
+copy c:\crobots\%tibbe%.c c:\crobots\tibbeCrobot.r
+
+REM Uses the fnr.exe - Find And Replace Tool
+REM https://findandreplace.codeplex.com/
+
+REM Remove LineFeed, NewLine, TAB
+fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find "[\r\n\t]?" --replace ""
+
+REM Find comments and remove them. /* Stuff with Lazy find */
+fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find "\/\*.*?\*\/" --replace ""
+
+REM Find something with zero space or many, and alot of special caracters, and zero space or many, save the middle special character and use it in replace.
+fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find " *([+\-*\/=><;\(\)\{\}\[\]\,]) *" --replace "$1"
+
+REM find my varible names that should be like thisVarible, with camelCase, one or more a-z, zero or many a-Z, one or many A-Z, zero or many a-Z. save each and mix them with the replace command
+fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find "([a-z]+)([a-zA-Z]*)([A-Z]+)([a-zA-Z]*)" --replace "$4$2$3$1"
+
+REM find my functions that use PascalCase. simpler but same as above. 
+fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find "([A-Z]+)([a-zA-Z]*)" --replace "$2$1"
