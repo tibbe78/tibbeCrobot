e3240cecb96afc2f47905c8292f965c64b11f02b
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2017-05-09 07:21:51 +0200

    added | to special characters

--- obfuscate.bat
+++ obfuscate.bat
@@ -14,7 +14,7 @@ REM Find comments and remove them. /* Stuff with Lazy find */
 fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find "\/\*.*?\*\/" --replace ""
 
 REM Find something with zero space or many, and alot of special caracters, and zero space or many, save the middle special character and use it in replace.
-fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find " *([+\-*\/=><;\(\)\{\}\[\]\,]) *" --replace "$1"
+fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find " *([+\-*\/=><;\(\)\{\}\[\]\,\|]) *" --replace "$1"
 
 REM find my varible names that should be camelCase like thisVarible, one or more a-z, zero or many a-Z, one or many A-Z, zero or many a-Z. save each and mix them with the replace command
 fnr --cl --dir "c:\crobots" --fileMask "tibbeCrobot.r" --caseSensitive --useEscapeChars --useRegEx --silent --find "([a-z]+)([a-zA-Z]*)([A-Z]+)([a-zA-Z]*)" --replace "$4$2$3$1"
