db59a237f2abe1be87f68875846d3afa76c854ca
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2017-05-13 22:28:20 +0200

    simplified the names

--- UpdateBots.ps1
+++ UpdateBots.ps1
@@ -1,4 +1,4 @@
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile("https://raw.githubusercontent.com/kaaswe/crobots/master/janne7.r","C:\crobots\git-janne.r")
-$webclient.DownloadFile("https://raw.githubusercontent.com/kaaswe/crobots/master/dudepro.r","C:\crobots\git-dudepro.r")
-$webclient.DownloadFile("https://raw.githubusercontent.com/tibbe78/tibbeCrobot/master/tibbeCrobot.r","C:\crobots\git-tibbeCrobot.r")
+$webclient.DownloadFile("https://raw.githubusercontent.com/kaaswe/crobots/master/dudepro.r","C:\crobots\git-dude.r")
+$webclient.DownloadFile("https://raw.githubusercontent.com/tibbe78/tibbeCrobot/master/tibbeCrobot.r","C:\crobots\git-tibbe.r")
