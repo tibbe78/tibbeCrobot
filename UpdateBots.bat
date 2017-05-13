0e52338358585dbe6c47b6e4cb1752b8765d1884
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2017-05-13 22:27:33 +0200

    runs the matches also

--- UpdateBots.bat
+++ UpdateBots.bat
@@ -1,1 +1,8 @@
+@Echo off
+cls
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\crobots\UpdateBots.ps1'"
+
+crobots-win32 -m1000 git-tibbe.r git-dude.r >tibbeVSdude.txt
+crobots-win32 -m1000 git-janne.r git-tibbe.r >janneVStibbe.txt
+crobots-win32 -m1000 git-dude.r git-janne.r >dudeVSjanne.txt
+crobots-win32 -m1000 git-tibbe.r git-dude.r git-janne.r >tibbeVSdudeVSjanne.txt
