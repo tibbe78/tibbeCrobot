9f0452998b094c9e1ae88b71db28089b55e1154d
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2017-05-14 14:23:22 +0200

    change name to match

--- UpdateBots.bat
+++ UpdateBots.bat
@@ -2,7 +2,8 @@
 cls
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\crobots\UpdateBots.ps1'"
 
-crobots-win32 -m1000 git-tibbe.r git-dude.r >tibbeVSdude.txt
-crobots-win32 -m1000 git-janne.r git-tibbe.r >janneVStibbe.txt
-crobots-win32 -m1000 git-dude.r git-janne.r >dudeVSjanne.txt
-crobots-win32 -m1000 git-tibbe.r git-dude.r git-janne.r >tibbeVSdudeVSjanne.txt
+crobots-win32 -m1000 git-tibbe.r git-dude.r >Match-tibbeVSdude.txt
+crobots-win32 -m1000 git-janne.r git-tibbe.r >Match-janneVStibbe.txt
+crobots-win32 -m1000 git-dude.r git-janne.r >Match-dudeVSjanne.txt
+crobots-win32 -m1000 git-tibbe.r git-dude.r git-janne.r >Match-tibbeVSdudeVSjanne.txt
+
